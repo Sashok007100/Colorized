431e8f549f98bf3d7bcf4e18b23fd408ea1595e5
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-08-09 22:05:07 +0300

    Initial Commit

---
+++ Colorized/ColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp.swift
+//  Colorized
+//
+//  Created by Alexandr Artemov (Mac Mini) on 09.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colorized/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Colorized
+//
+//  Created by Alexandr Artemov (Mac Mini) on 09.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
